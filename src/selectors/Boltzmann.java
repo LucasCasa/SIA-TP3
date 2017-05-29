008e498f576b1358bbbe3b5e4f8b2472dee96be6
nicokuyum <nicokuyum@hotmail.com>
2017-05-29 17:34:39 -0300

    Boltzmann usa función exponencial para la temperatura

--- src/selectors/Boltzmann.java
+++ src/selectors/Boltzmann.java
@@ -9,18 +9,17 @@ import interfaces.Selector;
  */
 public class Boltzmann implements Selector {
 
-    private double T;
-    private int generation; // T varies according to generation (more g less t)
-    private int jump;
+    private double k;
+    private double initial_temperature;
+    private int generation;
+    private final double minT = 0.1;
+    private boolean lessThanMin = false;
 
     public Boltzmann(){
-        T = Config.getInstance().getInteger("initial_temperature");
-        jump = Config.getInstance().getInteger("temperature_reduction");
-        if(T<=0){
-            throw new IllegalArgumentException("Illegal value for param initial_temperature");
-        }
-        if(jump<=0){
-            throw new IllegalArgumentException("Illegal value for param temperature_reduction");
+        k = Config.getInstance().getDouble("temperature_k");
+        initial_temperature = Config.getInstance().getDouble("initial_temperature");
+        if(initial_temperature<=0 || k<=0){
+            throw new IllegalArgumentException("Illegal values for temperature configuration");
         }
         generation = 1;
     }
@@ -35,8 +34,6 @@ public class Boltzmann implements Selector {
             ans[i] = selected;
         }
         generation++;
-        if(generation%jump==0 && T>=2)
-            T--;
         return ans;
     }
 
@@ -45,7 +42,7 @@ public class Boltzmann implements Selector {
         double avg = getAvgFitness(pop);
         double accumulated = 0;
         for(int i=0; i<pop.length; i++){
-            double relative = Math.exp(pop[i].getFitness()/T) / avg;
+            double relative = Math.exp(pop[i].getFitness()/getT()) / avg;
             accumulated = accumulated + relative;
             res[i] = accumulated;
         }
@@ -55,7 +52,7 @@ public class Boltzmann implements Selector {
     private double getAvgFitness(Phenotype[] pop){
         double total = 0;
         for(Phenotype p: pop)
-            total += Math.exp(p.getFitness()/T);
+            total += Math.exp(p.getFitness()/getT());
         return total/(double)pop.length;
     }
 
@@ -67,5 +64,14 @@ public class Boltzmann implements Selector {
         return null;
     }
 
-
+    private double getT(){
+        if(lessThanMin)
+            return minT;
+        double T = 100*Math.exp(-generation*k/100000);
+        if(T<minT) {
+            lessThanMin = true;
+            return minT;
+        }
+        return T;
+    }
 }
